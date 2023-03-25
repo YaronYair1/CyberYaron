7a85877d0dc7233169f2522882a08909ebf4412e
YaronYair1 <yaronyair10@gmail.com>
2023-03-25 23:33:47 +0200

    doesnt fully working

---
+++ cyberProjectSQL/client.py
@@ -0,0 +1,118 @@
+import socket
+import threading
+import tkinter as tk
+from tkinter import messagebox
+
+# specify host and port
+host = '127.0.0.1'
+port = 55555
+
+# create a socket object and connect to the server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+
+# define a function to handle incoming messages from the server
+def receive():
+    while True:
+        try:
+            # receive a message from the server
+            message = client.recv(1024).decode('ascii')
+
+            # if the message is "LOGIN", prompt the user for their username and password
+            if message == 'LOGIN':
+                username = username_entry.get()
+                password = password_entry.get()
+
+                # send the username and password to the server
+                client.send(username.encode('ascii'))
+                client.send(password.encode('ascii'))
+
+            # if the message is "FAIL", display an error message and close the connection
+            elif message == 'FAIL':
+                messagebox.showerror('Error', 'Invalid username or password.')
+                client.close()
+                break
+
+            # if the message is "SUCCESS", enable the message entry field and display the message history
+            elif message == 'SUCCESS':
+                message_entry.config(state='normal')
+                send_button.config(state='normal')
+                username_entry.config(state='disabled')
+                password_entry.config(state='disabled')
+
+                # receive and display the message history
+                while True:
+                    history = client.recv(1024).decode('ascii')
+                    if history == 'END':
+                        break
+                    messages_text.insert(tk.END, f'{history}\n')
+                    messages_text.yview(tk.END)
+
+            # otherwise, display the message in the chat window
+            else:
+                messages_text.insert(tk.END, f'{message}\n')
+                messages_text.yview(tk.END)
+
+        except:
+            # if an error occurs, close the connection and break the loop
+            client.close()
+            break
+
+
+# define a function to send messages to the server
+def send():
+    message = message_entry.get()
+    if message != '':
+        client.send(message.encode('ascii'))
+        message_entry.delete(0, tk.END)
+
+
+# create the main window and widgets
+root = tk.Tk()
+root.title('Chat App')
+root.geometry('400x400')
+
+messages_frame = tk.Frame(root)
+messages_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+messages_text = tk.Text(messages_frame)
+messages_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+scrollbar = tk.Scrollbar(messages_frame)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+messages_text.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=messages_text.yview)
+
+entry_frame = tk.Frame(root)
+entry_frame.pack(side=tk.BOTTOM, fill=tk.X)
+
+username_label = tk.Label(entry_frame, text='Username:')
+username_label.pack(side=tk.LEFT, padx=5)
+
+username_entry = tk.Entry(entry_frame)
+username_entry.pack(side=tk.LEFT, padx=5)
+
+password_label = tk.Label(entry_frame, text='Password:')
+password_label.pack(side=tk.LEFT, padx=5)
+
+password_entry = tk.Entry(entry_frame, show='*')
+password_entry.pack(side=tk.LEFT, padx=5)
+
+message_entry = tk.Entry(entry_frame)
+message_entry.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=5)
+
+send_button = tk.Button(entry_frame, text='Send', command=send, state='disabled')
+send_button.pack(side=tk.LEFT, padx=5)
+
+message_entry.bind('<Return>', lambda e: send())
+
+# start a thread to handle incoming messages from the server
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+
+# Press the green button in the gutter to run the script.
+def main():
+    root.mainloop()
---
+++ cyberProjectSQL/server.py
@@ -0,0 +1,127 @@
+import socket
+import threading
+import sqlite3
+import datetime
+
+# specify host and port
+host = '127.0.0.1'
+port = 55555
+clients = []
+
+# create a socket object
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+
+# create a database connection
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+
+# create the users table if it does not exist
+cursor.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)')
+
+# create the messages table if it does not exist
+cursor.execute('CREATE TABLE IF NOT EXISTS messages (sender TEXT, receiver TEXT, message TEXT, timestamp TEXT)')
+
+# commit changes and close connection
+conn.commit()
+conn.close()
+
+
+# define a function to handle client connections
+def handle_client(client_socket, client_address):
+    print(f'New connection from {client_address}')
+
+    # send a message to the client requesting the username and password
+    client_socket.send('LOGIN'.encode('ascii'))
+
+    # receive the username and password from the client
+    username = client_socket.recv(1024).decode('ascii')
+    password = client_socket.recv(1024).decode('ascii')
+
+    # authenticate the user
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
+    user = cursor.fetchone()
+    conn.close()
+
+    # if the user is not authenticated, close the connection
+    if user is None:
+        print(f'Authentication failed for {client_address}')
+        client_socket.send('FAIL'.encode('ascii'))
+        client_socket.close()
+        return
+
+    # if the user is authenticated, send a success message and the message history
+    print(f'{username} authenticated from {client_address}')
+    client_socket.send('SUCCESS'.encode('ascii'))
+
+    conn = sqlite3.connect('messages.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM messages WHERE receiver=? ORDER BY timestamp ASC', (username,))
+    messages = cursor.fetchall()
+    conn.close()
+
+    for message in messages:
+        sender, receiver, message_text, timestamp = message
+        client_socket.send(f'{sender}: {message_text}'.encode('ascii'))
+
+    # start a loop to receive and broadcast messages
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('ascii')
+
+            # if the message is "QUIT", close the connection and break the loop
+            if message == 'QUIT':
+                client_socket.close()
+                break
+
+            # otherwise, broadcast the message to all connected clients
+            print(f'{username}: {message}')
+            conn = sqlite3.connect('messages.db')
+            cursor = conn.cursor()
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cursor.execute('INSERT INTO messages (sender, receiver, message, timestamp) VALUES (?, ?, ?, ?)',
+                           (username, 'all', message, timestamp))
+            conn.commit()
+            conn.close()
+            broadcast_message = f'{username}: {message}'
+            for client in clients:
+                if client != client_socket:
+                    client.send(broadcast_message.encode('ascii'))
+        except:
+            # if an error occurs, close the connection and break the loop
+            print(f'Connection closed for {client_address}')
+            client_socket.close()
+            break
+
+
+# define a function to broadcast a message to all connected clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+
+
+def main():
+    # listen for incoming connections
+    server.listen()
+
+    print(f'Server running on {host}:{port}')
+
+    while True:
+        # accept a new connection
+        client, address = server.accept()
+        print(f'New connection from {address}')
+
+        # add the new client to the list
+        clients.append(client)
+
+        # create a new thread to handle the client
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+
+if __name__ == '__main__':
+    main()
